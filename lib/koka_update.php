7405d43a24b2af913234b6c60314627485c2c96e
Alexander Schulze <github@lxhome.de>
2025-04-29 21:37:18 +0200

    full link (with domain) in DB

--- lib/koka_update.php
+++ lib/koka_update.php
@@ -55,7 +55,7 @@ class koka_update extends SQLite3
 
             foreach ( pq ( '.event_box' ) as $event )
             {
-                $link   = '/' . pq ( '.button_view a', pq ( $event ) ) -> attr ( 'href' );
+                $link   = 'https://www.koka36.de/' . pq ( '.button_view a', pq ( $event ) ) -> attr ( 'href' );
                 $artist = trim ( pq ( '.nailthumb-container > img', pq ( $event )) -> attr ( 'alt' ) );
 
                 // sometimes there's no artist, we skip those entries
